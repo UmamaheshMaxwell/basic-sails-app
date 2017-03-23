c25051e5e9749c4bf7e4453c81eb5edc4cca47d3
UmamaheswararaoMeka <maheshmeka16@gmail.com>
2017-03-23 17:54:39 +0530

    Add CRUD functionality

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -6,7 +6,7 @@
  */
 
 module.exports = {
-	new : function(req, res){
+	new: function(req, res){
 		res.view();
 	},
 
@@ -20,7 +20,6 @@ module.exports = {
 	},
 
 	show: function(req, res){
-
 		Customer.findOne(req.params.id, function(err, customer){
 			if(err){
 				next(err)
@@ -32,6 +31,49 @@ module.exports = {
 				customer : customer
 			})
 		});
+	},
+
+	index: function(req, res, next){
+		Customer.find(function(err, customers){
+			if(err){
+				return next(err)
+			}
+			res.view({
+				customers : customers
+			})
+		})
+	},
+
+	edit: function(req, res, next){
+		Customer.findOne(req.params.id, function(err, customer){
+			if(err){
+				next(err)
+			}
+			if(!customer){
+				return next();
+			}
+			res.view({
+				customer : customer
+			})
+		});
+	},
+
+	update: function(req, res, next){
+		Customer.update(req.params.id, req.params.all(), function(err, customer){
+			if(err){
+				return res.redirect("/customer/edit/" + req.params.id)
+			}
+
+			res.redirect("/customer/show/"+ req.params.id)
+		});
+	},
+
+	destroy: function(req, res, next){
+		Customer.destroy(req.params.id).exec(function(){
+			res.redirect("/customer/")
+		})
 	}
+
+
 };
 
