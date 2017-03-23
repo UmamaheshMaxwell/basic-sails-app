2fc56e773e372c302fa126b669ee370240d7c8d9
UmamaheswararaoMeka <maheshmeka16@gmail.com>
2017-03-23 13:40:40 +0530

    Add customer creation and display

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -6,6 +6,32 @@
  */
 
 module.exports = {
-	
+	new : function(req, res){
+		res.view();
+	},
+
+	create: function(req, res, next){
+		Customer.create(req.params.all(),function(err, customer){
+			if(err){
+				next(err);
+			}
+			res.redirect("/customer/show/" + customer.id)
+		})
+	},
+
+	show: function(req, res){
+
+		Customer.findOne(req.params.id, function(err, customer){
+			if(err){
+				next(err)
+			}
+			if(!customer){
+				return next();
+			}
+			res.view({
+				customer : customer
+			})
+		});
+	}
 };
 
