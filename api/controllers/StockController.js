ec1cd9ea38f413c1959decc90d7c848640c19a11
UmamaheswararaoMeka <maheshmeka16@gmail.com>
2017-03-24 16:16:40 +0530

    Add Stock Model

---
+++ api/controllers/StockController.js
@@ -0,0 +1,31 @@
+/**
+ * StockController
+ *
+ * @description :: Server-side logic for managing stocks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	'new' : function(req, res, next){		
+		Customer.findOne(req.param('owner'), function(err, customer){
+			if(err){
+				next(err)
+			}
+			res.view({
+				customer : customer
+			})
+		});
+	},
+
+	create: function(req, res, next){
+		Stock.create(req.params.all(), function (err, stock){
+		  if (err) { 
+		  	return next(err); 
+		  }
+		   res.json(stock)
+		});
+	}
+
+};
+
